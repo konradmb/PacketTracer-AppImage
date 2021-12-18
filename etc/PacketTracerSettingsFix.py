155f7f78a13219e89051ee61848753d9a740b850
konradmb <konradmb@o2.pl>
2021-12-18 01:25:26 +0100

    Update to 8.1.0

--- etc/PacketTracerSettingsFix.py
+++ etc/PacketTracerSettingsFix.py
@@ -1,29 +1,38 @@
-import os, sys
+import os
+import sys
 
-homePath = os.getenv("HOME")
-packetTracerHomePath = homePath + "/PacketTracer"
 
-settingsFilePath = homePath + "/.ptappimage00"
-settingsFile = open(settingsFilePath, "wb")
+def write_pt_file(pathStartChar, pathSeparatorChar, packetTracerRoot, settingsFileName, homeFolder):
+    homePath = os.getenv("HOME")
+    packetTracerHomePath = homePath + homeFolder
 
-if sys.argv[1] == "7.3":
-  fileHeader = bytearray([0x00,0x00,0x00,0x01,0x00,0x00,0x00])
-  pathStart =  bytearray(">", "utf16")[2:]
-  pathSeparator = bytearray([0x00,0x00,0x00,0x32, 0x00])
-  packetTracerPath = os.getcwd() + "/opt/pt"
+    settingsFilePath = homePath + settingsFileName
+    settingsFile = open(settingsFilePath, "wb")
+
+    fileHeader = bytearray([0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00])
+    if type(pathStartChar) == str:
+        pathStart = bytearray(pathStartChar, "utf16")[2:]
+    else:
+        pathStart = bytearray(pathStartChar)
+    pathSeparator = bytearray([0x00, 0x00, 0x00, pathSeparatorChar, 0x00])
+    packetTracerPath = os.getcwd() + packetTracerRoot
+
+    packetTracerPathEncoded = bytearray(packetTracerPath, "utf16")[2:-1]
+    packetTracerHomePathEncoded = bytearray(
+        packetTracerHomePath, "utf16")[2:-1]
+    encodedPaths = pathStart + packetTracerPathEncoded \
+        + pathSeparator + packetTracerHomePathEncoded
+    encodedPaths = encodedPaths
+
+    bytesToWrite = fileHeader + encodedPaths
+    settingsFile.write(bytesToWrite)
+    settingsFile.close()
+
+
+if sys.argv[1] == "8.1":
+    write_pt_file([0x82, 0x00], 0x1E, "/opt/pt", "/.ptappimage00", "/pt")
+elif sys.argv[1] == "7.3":
+    write_pt_file(">", 0x32, "/opt/pt", "/.ptappimage00", "/PacketTracer")
 elif sys.argv[1] == "7.2":
-  fileHeader = bytearray([0x00,0x00,0x00,0x01,0x00,0x00,0x00])
-  pathStart =  bytearray("^", "utf16")[2:]
-  pathSeparator = bytearray([0x00,0x00,0x00,0x32, 0x00])
-  packetTracerPath = os.getcwd() + "/share/PacketTracer"
-
-
-packetTracerPathEncoded = bytearray(packetTracerPath, "utf16")[2:-1]
-packetTracerHomePathEncoded = bytearray(packetTracerHomePath, "utf16")[2:-1]
-encodedPaths = pathStart + packetTracerPathEncoded \
-            + pathSeparator + packetTracerHomePathEncoded
-encodedPaths = encodedPaths
-
-bytesToWrite = fileHeader + encodedPaths 
-settingsFile.write(bytesToWrite)
-settingsFile.close()
+    write_pt_file("^", 0x32, "/share/PacketTracer",
+                  "/.ptappimage00", "/PacketTracer")
